da6364f19e41d7a68a540f38fc6c8fbe56064259
303B-20\cre <a>
2023-07-18 19:26:46 +0900

    未完成

---
+++ kadai1/q10.php
@@ -0,0 +1,10 @@
+<!-- 問題 10（初級）:
+変数 grade（成績）があります。grade が 90 以上なら「You got an A」、80 以上 90 未満なら「You got a B」、70 以上 80 未満なら「You got a C」、60 以上 70 未満なら「You got a D」、それ以外の場合は「You got an F」と表示する PHP プログラムを作成してください。-->
+<?php
+$var = "grade";
+
+if ($var = $num == 80)
+if ($num >= 90)
+echo "You got an A"
+
+?>
