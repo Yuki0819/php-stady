3192a1ccf5c29d2c03324b5e1a5458b23c89c16d
BuildTools <unconfigured@null.spigotmc.org>
2023-07-20 09:38:35 +0900

    未完成

--- kadai1/q8.php
+++ kadai1/q8.php
@@ -1,5 +1,10 @@
 <!-- 問題 8（上級）:
 年齢を表す変数 age があります。年齢が 18 歳以上かつ運転免許を持つ変数 hasDrivingLicense が true であれば、「You can drive.」、そうでなければ「You cannot drive.」と表示する PHP プログラムを作成してください。-->
 <?php
-$var = "hasDrivingLicense";
+$var = "hasDrivingLicense"; $num = 18;
+if ($var and >= 18) {
+    echo "You can drive.\n";
+} else { 
+    echo "You cannot drive.\n";
+}
 ?>
--- kadai1/q9.php
+++ kadai1/q9.php
@@ -1,5 +1,12 @@
 <!-- 問題 9（上級）:
 2 つの変数 temp（温度）と isRaining（雨が降っているか）があります。もし温度が 0 度以下で雨が降っている場合は「It may snow.」、そうでなければ「It is unlikely to snow.」と表示する PHP プログラムを作成してください。-->
 <?php
-$var1 = "temp"; $var2 = "isRaining";
+$num = 0; $var = "isRaining";
+
+if ($num >= 0 && $var) {
+    echo "It may snow.\n";
+} else {
+    echo "It is unlikely to snow.\n";
+}
+
 ?>
