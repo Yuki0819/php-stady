9804db8705131b913cec68c2152c1937e3ec9b7a
303B-20\cre <a>
2023-07-25 19:21:52 +0900

    no message

--- command.php
+++ command.php
@@ -8,3 +8,16 @@ php ./kadai1/q7.php
 php ./kadai1/q8.php
 php ./kadai1/q9.php
 php ./kadai1/q10.php
+
+php q1.php
+php q2.php
+php q3.php
+php q4.php
+php q5.php
+php q6.php
+php q7.php
+php q8.php
+php q9.php
+php q10.php
+
+開く場所によってコマンドも違う
