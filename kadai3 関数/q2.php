ecb4f4a31b80e4c322076e1cf5ee2cbb5a7dc549
303B-20\cre <a>
2023-08-29 19:28:44 +0900

    関数の課題始まり

---
+++ kadai3 関数/q2.php
@@ -0,0 +1,10 @@
+### 問題 2: 引数を使った挨拶
+
+- **要件**: 引数に名前を取り、"Hello, [名前]!"と出力する関数`sayHelloTo`を定義してください。
+<?php
+sayHello("名前");
+
+function sayHello($sayHello){
+echo ("Hello" + $sayHello + "!");
+}
+?>
