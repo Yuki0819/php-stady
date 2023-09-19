05987f0e8946914ed056d063fa1b83f5a32b00a4
303B-20\cre <a>
2023-09-19 19:28:21 +0900

    class問題４はまだ２，３は動くが、、、、

---
+++ kadai4/Gun.php
@@ -0,0 +1,63 @@
+<?php
+class Gun {
+
+    // ↓フィールド============================
+    // 銃の名前
+    private $name;
+    // 最大装弾数
+    private $maxMagazine;
+    // 残弾
+    private $currentMagazine;
+    // ↑フィールド============================
+
+    // コンストラクタ
+    function __construct($name, $maxMagazine) {
+        // 問題1
+        $this->name = $name;
+        $this->maxMagazine = $maxMagazine;
+        $this->currentMagazine = 0;
+    }
+
+    // 現在の状態を表示
+    function echoStatus() {
+        echo "======現在の状態======". "\n";
+        echo "武器名: " . $this->name . "\n";
+        echo "最大装弾数: " . $this->maxMagazine . "\n";
+        echo "残弾数: " . $this->currentMagazine . "\n";
+        echo "======================". "\n";
+    }
+
+
+    // リロード
+    function relaod() {
+        // 問題2
+        if ($this->currentMagazine = $this->maxMagazine) {
+            $this->maxMagazine == $this->currentMagazine;
+                echo "リロードの必要はありません\n";
+              }
+    }
+
+    // 発砲
+    function fire() {
+        // 問題3
+        if ($this->currentMagazine == 0) {
+            echo "リロードしてください\n";
+        } else {
+            $this->currentMagazine --;
+            echo "[$this->name]を発砲しました。残弾: [$this->currentMagazine] 発\n";
+            if ($this->currentMagazine == 0) {
+                echo "リロードしてください\n";
+            }
+        } 
+    }
+
+    // 拡張マガジンを装着
+    function setExtendedMagazine() {
+        // 問題4
+    }
+
+    // 拡張マガジンを取外し
+    function unsetExtendedMagazine() {
+        // 問題4
+    }
+}
---
+++ kadai4/Main.php
@@ -0,0 +1,20 @@
+<?php
+
+// Userクラスの読み込み
+require 'Gun.php';
+
+$mainWeapon = new Gun("AK47",30);
+$subWeapon = new Gun("Mosin-Nagant",5);
+
+// 現在の状態を表示
+$mainWeapon->echoStatus();
+$subWeapon->echoStatus();
+
+$mainWeapon->relaod();
+$subWeapon->relaod();
+
+$mainWeapon->fire();
+$subWeapon->fire();
+
+$mainWeapon->echoStatus();
+$subWeapon->echoStatus();
